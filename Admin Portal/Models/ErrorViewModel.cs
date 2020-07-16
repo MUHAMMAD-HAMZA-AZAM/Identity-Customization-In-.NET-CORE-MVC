8abcf991a8fb8c8077136cb1415ddaa15d851454
muhammadhamzabutt80@gmail.com <muhammadhamzabutt80@gmail.com>
2020-07-17 02:03:56 +0500

    Add project files.

---
+++ Admin Portal/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Admin_Portal.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
